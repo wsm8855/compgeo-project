dda1b86efae00da7fa42b3f6e383a10890e62a33
wsm8855 <wsm8855@g.rit.edu>
2022-12-04 01:15:51 -0500

    Add security check for request parameters

--- devserver/devserver.go
+++ devserver/devserver.go
@@ -12,6 +12,7 @@ import (
 var DEV_MODE = true
 var SCRIPT_PATH = "../rupperts.py"
 var PYTHON_PATH = "python3"
+var ALLOWED_CHARS = "0123456789.,"
 
 var DEBUG bool = true
 
@@ -21,11 +22,73 @@ func debug(to_print ...interface{}) {
 	}
 }
 
+func isin(char rune, allowed string) bool {
+	char_in_allowed := false
+	for _, allowed_char := range allowed {
+		if char == allowed_char {
+			char_in_allowed = true
+			break
+		}
+	}
+	return char_in_allowed
+}
+
+func safe_str(unsafe_str string, allowed_chars string) bool {
+	for _, unsafe_char := range unsafe_str {
+		if !isin(unsafe_char, allowed_chars) {
+			return false
+		}
+	}
+	return true // implies string is safe
+}
+
+func request_is_safe(r *http.Request) bool {
+	q := r.URL.Query()
+	unsafe_points := q.Get("points")
+	unsafe_refine := q.Get("refine")
+
+	security_checks_passed := true
+	if !safe_str(unsafe_points, ALLOWED_CHARS) {
+		fmt.Println("Security hazard detected in points argument: " + unsafe_points)
+		security_checks_passed = false
+	}
+	if !safe_str(unsafe_refine, ALLOWED_CHARS) {
+		fmt.Println("Security hazard detected in refine argument: " + unsafe_refine)
+		security_checks_passed = false
+	}
+
+	if !security_checks_passed {
+		return false
+	}
+
+	unsafe_angle := ""
+	unsafe_length := ""
+	if unsafe_refine == "1" {
+		unsafe_angle = q.Get("angle")
+		unsafe_length = q.Get("length")
+	}
+	if len(unsafe_angle) > 0 && !safe_str(unsafe_angle, ALLOWED_CHARS) {
+		fmt.Println("Security hazard detected in angle argument: " + unsafe_angle)
+		security_checks_passed = false
+	}
+	if len(unsafe_length) > 0 && !safe_str(unsafe_length, ALLOWED_CHARS) {
+		fmt.Println("Security hazard detected in length argument: " + unsafe_length)
+		security_checks_passed = false
+	}
+	return security_checks_passed
+}
+
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
 
 func ajaxHandler(w http.ResponseWriter, r *http.Request) {
+	// SECURITY CHECK DO NOT REMOVE
+	if !request_is_safe(r) {
+		http.Error(w, "Error 404 not found", http.StatusNotFound)
+		return
+	}
+	// ALL INPUTS SAFE
 	if !DEV_MODE {
 		enableCors(&w)
 	}
@@ -35,7 +98,7 @@ func ajaxHandler(w http.ResponseWriter, r *http.Request) {
 	refine := q.Get("refine")
 	angle := ""
 	length := ""
-	if refine == "1" {
+	if refine == "1" { // if logic is changed, refer to security check to ensure validity
 		angle = q.Get("angle")
 		length = q.Get("length")
 	}
